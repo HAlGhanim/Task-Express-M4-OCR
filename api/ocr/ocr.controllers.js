41c6083ab32e9b7bf9b82bbecec42ae8981c56d6
HAlGhanim <imzaji98@gmail.com>
2023-06-20 13:43:56 +0300

    done with all 3 bonuses for M4 - image to text using tessaract, multer file size, multer fileFilter to ensure only images are uploaded

--- api/ocr/ocr.controllers.js
+++ api/ocr/ocr.controllers.js
@@ -1,7 +1,16 @@
+const tesseract = require("tesseract.js");
+
+tesseract
+  .recognize("http://localhost:8000/media/1687257691539hw.png", "eng")
+  .then(({ data: { text } }) => {
+    console.log(text);
+  });
+
 exports.ocrCreate = async (req, res, next) => {
   try {
     if (req.file) {
-      req.body.image = `${req.file.path}`;
+      req.body.image = `http://${req.get("host")}/media/${req.file.filename}`;
+      console.log(req.body.image);
     }
     res.status(201).json(req.file);
   } catch (error) {
--- middlewares/multer.js
+++ middlewares/multer.js
@@ -9,6 +9,19 @@ const storage = multer.diskStorage({
 
 const upload = multer({
   storage,
+  fileFilter: (req, file, cb) => {
+    if (
+      file.mimetype === "image/jpeg" ||
+      file.mimetype === "image/jpg" ||
+      file.mimetype === "image/png"
+    ) {
+      cb(null, true);
+    } else {
+      cb(null, false);
+      return cb(new Error("Only JPG, JPEG, and PNG images are supported"));
+    }
+  },
+  limits: { fileSize: 1000000 },
 });
 
 module.exports = upload;
